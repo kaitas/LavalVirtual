57f4f412479ed821b0f41da249bf271f6512568d
Akihiko Shirai <akihiko.shirai@gree.net>
2022-12-26 04:18:39 +0900

    next

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,20 +50,6 @@ export default function Home() {
           </a>
 
           <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={styles.container}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={styles.container}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
             href="https://github.com/kaitas/ReVolution"
             className={styles.card}
             target="_blank"
@@ -77,20 +63,6 @@ export default function Home() {
             </p>
           </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={styles.container}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={styles.container}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
         </div>
       </main>
     </>
