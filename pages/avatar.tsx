81927a8961798bcdc3b04e16511c6c71df25a08e
Akihiko SHIRAI, Ph.D <akihiko.shirai@gree.net>
2022-12-27 10:59:41 +0900

    Create avatar.tsx

---
+++ pages/avatar.tsx
@@ -0,0 +1,8 @@
+import Link from 'next/link';
+import Image from 'next/image'
+
+function Avatar() {
+  return <Image src="/HakaseQR.png" alt="me" width="64" height="64" />
+}
+
+export default Avatar
