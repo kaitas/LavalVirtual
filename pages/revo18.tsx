d962c8a4afc8436c944c2a2f3a7b51473663d2b5
Akihiko SHIRAI, Ph.D <akihiko.shirai@gree.net>
2022-12-27 11:00:08 +0900

    Create revo18.tsx

---
+++ pages/revo18.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import Image from 'next/image'
+export default function ReVo18() {
+    return (
+      <>
+        <h1>Laval Virtual ReVolution 2018</h1>
+        <h2>
+          <Link href="/">Back to home</Link>
+        </h2>
+      </>
+    );
+  }
+ 
